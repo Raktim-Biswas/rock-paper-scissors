2b97d4d35e1a9c875be2705487fd5b870555f5c2
Raktim-Biswas <biswasraktim24@gmail.com>
2024-06-10 20:25:29 +0530

    add game logic for single round

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Computer choice logic
+// computer choice logic
 function getComputerChoice() {
   let randomNumber = (Math.random() * 100 + 1);
   if (randomNumber <= 1/3 * 100) {
@@ -10,17 +10,62 @@ function getComputerChoice() {
   }
 }
 
-// Human choice logic
+// human choice logic
 function getHumanChoice() {
-    let humanChoice = prompt("Pick 'Rock', 'Paper' or 'Scissors'.");
-    humanChoice = humanChoice.toLowerCase();
-    console.log(humanChoice);
-    return humanChoice
+    let userInput = prompt("Pick 'Rock', 'Paper' or 'Scissors'.");
+    userInput = userInput.toLowerCase();
+    console.log(userInput);
+    return userInput
 }
 
 // player scores
 humanScore = 0;
 computerScore = 0;
+
+// single round logic
+function playRound(humanChoice, computerChoice) {
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+
+  if (computerChoice === "rock") {
+    if (humanChoice === "rock") {
+      console.log("You both played Rock! It's a tie!")
+    } else if (humanChoice === "paper") {
+      console.log("You win! Paper beats Rock!");
+      humanScore++;
+    } else if (humanChoice === "scissors") {
+      console.log("You lose! Rock beats scissors!");
+      computerScore++;
+    }
+  } 
+
+  if (computerChoice === "paper") {
+    if (humanChoice === "rock") {
+      console.log("You lose! Paper beats Rock");
+      computerScore++;
+    } else if (humanChoice === "paper") {
+      console.log("It's a tie! You both played Paper")
+    } else if (humanChoice === "scissors") {
+      console.log("You win! Scissors beats Paper");
+      humanScore++;
+  }
+} 
+
+  if (computerChoice === "scissors") {
+    if (humanChoice === "rock") {
+      console.log("You win! Rock beats Scissors");
+      humanScore++;
+    } else if (humanChoice === "paper") {
+      console.log("You lose! Scissors beat Paper");
+      computerScore++;
+    } else if (humanChoice === "scissors") {
+      console.log("It's a tie! You both picked Scissors!")
+    }   
+  }
+}
+
+
 
 
 
